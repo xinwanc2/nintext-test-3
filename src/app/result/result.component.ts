c89046d5781eb2c9d389c0ea9b9c830e30952129
xinwanc2 <yuki.madelynn1004@outlook.com>
2022-02-21 08:26:31 +0800

    1 file modified

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    AfterViewChecked,
+    AfterViewInit,
+    Component,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable, of } from 'rxjs';
 import { FlightDataService, Itinerary } from '../flight-data.service';
 
@@ -7,12 +15,25 @@ import { FlightDataService, Itinerary } from '../flight-data.service';
     templateUrl: './result.component.html',
     styleUrls: ['./result.component.scss'],
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, AfterViewInit {
     itineraries$: Observable<Itinerary[]> = of([]);
+    displayedColumns = ['airline', 'stops', 'inbound', 'outbound', 'total'];
+    dataSource!: MatTableDataSource<Itinerary>;
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
 
     constructor(private dataService: FlightDataService) {}
 
     ngOnInit(): void {
         this.itineraries$ = this.dataService.getItineraries();
+        this.dataService
+            .getItineraries()
+            .subscribe((itineries: Itinerary[]) => {
+                this.dataSource = new MatTableDataSource<Itinerary>(itineries);
+            });
+    }
+
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
     }
 }
